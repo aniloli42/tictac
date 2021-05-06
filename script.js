9566dcfb00f6265710c4421adbe3f132423fded1
aniloli42 <aniloli42@gmail.com>
2021-05-07 00:33:42 +0545

    changes the interface and codes

--- script.js
+++ script.js
@@ -1,59 +1,43 @@
-//  Click Box finder
-let noBoxMarked = 0;
 let columns = document.getElementsByTagName("td");
+let selectCurrentClass = "redMark";
+let turn = 0;
+let turnText = document.querySelector(".playerTurn");
 
-Array.from(columns).forEach(function (element) {
-  if (noBoxMarked < 9) {
-    element.addEventListener("click", function (e) {
-      // Only Box mark when box is blank and have id
-      if (e.target.children.length == 0 && e.target.id != "") {
-        // Check the no of Box checked
-        if (noBoxMarked < 9) {
-          noBoxMarked += 1;
-          // Calling the Box mark
-          mark(e.target.id);
-        }
-      }
-    });
-  }
+// Click Box Finder
+Array.from(columns).forEach((element) => {
+  element.addEventListener("mouseup", (e) => {});
+  element.addEventListener("click", (e) => {
+    let cell = e.target;
+    mark(cell, selectCurrentClass);
+    if (winStatus(selectCurrentClass)) {
+      showWinMessage(false);
+    } else if (checkDraw()) {
+      showWinMessage(true);
+    } else {
+      playTurn();
+    }
+  });
 });
 
 //  Mark Code
-function mark(id) {
-  // Select the box with specific id
-  let clickedBox = document.getElementById(id);
-  if (clickedBox.children.length == "0") {
-    if (turn == 0) {
-      clickedBox.innerHTML = `<div class="circleDesign" style="border-color:red;background-color:red; "></div>`;
-    } else {
-      clickedBox.innerHTML = `<div class="circleDesign"  style="border-color:yellow;background-color:yellow; "></div>`;
-    }
-    playTurn(Number(id));
+function mark(cell, selectCurrentClass) {
+  if (
+    !cell.classList.contains("redMark") &&
+    !cell.classList.contains("yellowMark")
+  ) {
+    cell.classList.add(selectCurrentClass);
   }
 }
 
 // Player Turn
-let firstPlayer = 0;
-let secondPlayer = 1;
-let firstPlayerSelect = [];
-let secondPlayerSelect = [];
-let turn = 0;
-let turnText = document.querySelector(".playerTurn");
-
-function playTurn(id) {
-  if (turn == 0) {
-    turn = 1;
-    firstPlayerSelect.push(id);
-    turnText.innerText = `Player Turn: Second Player`;
-  } else {
-    turn = 0;
-    secondPlayerSelect.push(id);
-    turnText.innerText = `Player Turn: First Player`;
-  }
-  winStatus();
+function playTurn() {
+  turn = !turn;
+  selectCurrentClass = turn == 0 ? "redMark" : "yellowMark";
+  turnText.innerText =
+    turn == 0 ? "Player Turn: First Player" : "Player Turn: Second Player";
 }
 
-// Win Status
+// Win Check
 let winCondition = [
   [0, 1, 2],
   [3, 4, 5],
@@ -64,41 +48,46 @@ let winCondition = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-let firstWinChance;
-let secondWinChance;
-const winMessage = document.querySelector("[data-message]");
-function winStatus() {
-  winCondition.forEach((winItem) => {
-    firstWinChance = winItem.every((number) =>
-      firstPlayerSelect.includes(number)
-    );
-    secondWinChance = winItem.every((number) =>
-      secondPlayerSelect.includes(number)
-    );
-    if (firstWinChance === true) {
-      winMessage.children[0].innerText = `First Wins!`;
-      winMessage.style.display = "flex";
-    } else if (secondWinChance === true) {
-      winMessage.children[0].innerText = `Second Wins!`;
-      winMessage.style.display = "flex";
-    } else if (noBoxMarked == 9 && !firstWinChance && !secondWinChance) {
-      winMessage.children[0].innerText = `Draw!`;
-      winMessage.style.display = "flex";
-    }
+function winStatus(selectCurrentClass) {
+  return winCondition.some((winCombination) => {
+    return winCombination.every((number) => {
+      return columns[number].classList.contains(selectCurrentClass);
+    });
   });
+}
+
+// Check Draw
+
+function checkDraw() {
+  return [...columns].every((element) => {
+    return (
+      element.classList.contains("redMark") ||
+      element.classList.contains("yellowMark")
+    );
+  });
+}
+
+// Win Show
+const winMessage = document.querySelector("[data-message]");
+function showWinMessage(draw) {
+  if (draw) {
+    winMessage.children[0].innerText = `Draw!`;
+    winMessage.style.display = "flex";
+  } else {
+    winMessage.children[0].innerText = `${
+      selectCurrentClass == "redMark" ? "Red Wins" : "Yellow Wins"
+    }`;
+    winMessage.style.display = "flex";
+  }
 }
 
 // Restart Game
 document.querySelector("[data-game-restart]").addEventListener("click", () => {
   winMessage.style.display = "none";
-  firstPlayerSelect = [];
-  secondPlayerSelect = [];
-  firstWinChance = "";
-  secondWinChance = "";
-  noBoxMarked = 0;
   turn = 0;
+  selectCurrentClass = "redMark";
   turnText.innerText = `Player Turn: First Player`;
   [...columns].forEach((element) => {
-    element.innerHTML = "";
+    element.className = "";
   });
 });
